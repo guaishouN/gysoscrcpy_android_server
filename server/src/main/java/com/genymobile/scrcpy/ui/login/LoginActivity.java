e502832b795039ed34ccbf70725208a07d57631e
GuaishouN <674149099@qq.com>
2024-07-03 16:44:47 +0800

    add test

---
+++ server/src/main/java/com/genymobile/scrcpy/ui/login/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.genymobile.scrcpy.ui.login;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.genymobile.scrcpy.R;
+public class LoginActivity extends Activity {
+    private static LoginActivity instance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        instance = this;
+        doLogin();
+    }
+
+    public static void doLogin() {
+        Log.d("TAG", "login");
+        Context context = instance.getApplication();
+        context.getAssets();
+    }
+}
+
+
+
+
+
