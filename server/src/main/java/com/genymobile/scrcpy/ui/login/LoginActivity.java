962cda4739de6d50e9cc4d0e93f88596420d2aca
GuaishouN <674149099@qq.com>
2024-07-03 21:21:56 +0800

    ss

--- server/src/main/java/com/genymobile/scrcpy/ui/login/LoginActivity.java
+++ server/src/main/java/com/genymobile/scrcpy/ui/login/LoginActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.genymobile.scrcpy.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class LoginActivity extends Activity {
     private static LoginActivity instance;
 
@@ -14,13 +20,25 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         instance = this;
-        doLogin();
+        check_token();
     }
 
     public static void doLogin() {
         Log.d("TAG", "login");
         Context context = instance.getApplication();
         context.getAssets();
+    }
+
+    public static void check_token() {
+        ExecutorService exe = Executors.newCachedThreadPool();
+        exe.submit(()->{
+            Calendar now = Calendar.getInstance();
+            now.add(Calendar.DAY_OF_YEAR, -1);
+            if (now.after(Utils.getTokenCreateTime())) {
+                Log.i("login","Token已经过期重新登录");
+                doLogin();
+            }
+        });
     }
 }
 
